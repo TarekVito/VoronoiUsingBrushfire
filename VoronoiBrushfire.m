8351027c906c6f31207cb329607a9d2084237b02
Tarek Mamdouh <tarek.mamdouh.cis@gmail.com>
2016-05-30 20:48:03 +0200

    Update VoronoiBrushfire.m

--- VoronoiBrushfire.m
+++ VoronoiBrushfire.m
@@ -1,6 +1,14 @@
-function [ res ] = VoronoiBrushfire( img )
+function [ res ,I] = VoronoiBrushfire( img )
 img = 1-im2bw(img);
+
+% considering the boundaries as an obstacle
+img(size(img,1),:) = 1;
+img(1,:) = 1;
+img(:,size(img,2)) = 1;
+img(:,1) = 1;
+
 I = getDistanceMap(img);
+imshow(I);
 [Gmag, ~] = imgradient(I');
 res = Gmag'./max(Gmag(:));
 imshow(res);
